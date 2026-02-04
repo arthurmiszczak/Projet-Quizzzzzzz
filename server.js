baaedce53a4320930b42c7b21af59ef3e70a7c37
arthurmiszczak <Arthur.arthur.miszczak@gmail.com>
2026-02-04 15:13:44 +0100

    Server

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3001;
+
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+const server = app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
+
+server.on('error', (err) => {
+    console.error('Erreur serveur:', err);
+    process.exit(1);
+});
