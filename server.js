7a88da341bab8e45adb1f8d54793672570f1cacc
arthurmiszczak <Arthur.arthur.miszczak@gmail.com>
2026-02-13 12:20:30 +0100

    Root register

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const connection = mysql.createConnection({
     host: '172.29.18.116',
     user: 'Demo',
     password: 'Demo',
-    database: 'BddArthur',
+    database: 'BddQuizz',
 });
 
 console.log('Connexion réussie à la base de données');
@@ -26,4 +26,24 @@ const server = app.listen(port, () => {
 server.on('error', (err) => {
     console.error('Erreur serveur:', err);
     process.exit(1);
+});
+
+
+app.post('/register', (req, res) => {
+    console.log('Données reçues pour l\'inscription');
+    console.log(req.body);
+
+    connection.query(
+      'INSERT INTO Users (login, password) VALUES (?, ?)',
+      [req.body.input, req.body.password],
+      (err, results) => {
+        if (err) {
+          console.error('Erreur lors de l\'insertion dans la base de données :', err);
+          res.status(500).json({ message: 'Erreur serveur' });
+          return;
+        }
+        console.log('Insertion réussie, ID utilisateur :', results.insertId);
+        res.json({ message: 'Inscription réussie !', userId: results.insertId });
+      }
+    );
 });
